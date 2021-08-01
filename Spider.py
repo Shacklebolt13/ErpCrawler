bab0d05b61b9780e6aba7234e9ab4f175dce8fa0
Shacklebolt13 <19cse223.gagandeepsinghbhambrah@giet.edu>
2021-08-01 17:01:53 +0530

    able to fetch the marks page

--- Spider.py
+++ Spider.py
@@ -2,6 +2,9 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 import sys
 import logging
+import json
+from scrapy.http.response.html import HtmlResponse
+
 
 class spiderForAttendance(scrapy.Spider):
     name='spiderForAttendance'
@@ -14,51 +17,93 @@ class spiderForAttendance(scrapy.Spider):
     __EVENTTARGET=""
     __EVENTARGUMENT=""
     start_urls=[attendanceSite]
+    data :dict
+    data={}
+    returnJson={}
+
 
     def __init__(self,u='u',p='p'):
         self.txtUserName=u
         self.txtPassword=p
         super(spiderForAttendance,self).__init__()
 
-    def parse(self,response):
+
+
+
+    def parse(self,response : HtmlResponse):
+        self.getBasicData(response)
+        self.data.update({'txtUserName' : self.txtUserName,'btnNext': self.btnNext })
+        yield scrapy.FormRequest(url=self.attendanceSite,formdata=self.data,callback=self.loginWithPassword)
+
+
+
+    def loginWithPassword(self,response : HtmlResponse):
+        self.getBasicData(response)
+        self.data.update({'txtPassword' : self.txtPassword,'btnSubmit' : self.btnSubmit})
+        print('__REACHED__')
+        yield scrapy.FormRequest(url=self.attendanceSite,formdata=self.data,callback=self.gotoSemMarksPage)
+
+
+    def getBasicData(self,response,reset=False):
+        self.data={} if reset else self.data
         self.__VIEWSTATEGENERATOR=response.css('input[name="__VIEWSTATEGENERATOR"]::attr(value)').extract_first()
         self.__EVENTVALIDATION=response.css('input[name="__EVENTVALIDATION"]::attr(value)').extract_first()
         self.__VIEWSTATE=response.css('input[name="__VIEWSTATE"]::attr(value)').extract_first()
-
         self.data={'__VIEWSTATEGENERATOR' : self.__VIEWSTATEGENERATOR,
                    '__EVENTVALIDATION' : self.__EVENTVALIDATION,
                    '__VIEWSTATE' : self.__VIEWSTATE,
                    '__LASTFOCUS' : self.__LASTFOCUS,
                    '__EVENTTARGET' : self.__EVENTTARGET,
                    '__EVENTARGUMENT' : self.__EVENTARGUMENT,
-                   'txtUserName' : self.txtUserName,
-                   'btnNext': self.btnNext
                   }
 
-        yield scrapy.FormRequest(url=self.attendanceSite,formdata=self.data,callback=self.put_pass)
+    def showOutHtml(self,response : HtmlResponse):
+        file=open('view.html','w+b')
+        file.write(response.body)
+        file.close
 
-    def put_pass(self,response):
 
-        self.__VIEWSTATEGENERATOR=response.css('input[name="__VIEWSTATEGENERATOR"]::attr(value)').extract_first()
-        self.__EVENTVALIDATION=response.css('input[name="__EVENTVALIDATION"]::attr(value)').extract_first()
-        self.__VIEWSTATE=response.css('input[name="__VIEWSTATE"]::attr(value)').extract_first()
+    def gotoSemMarksPage(self,response : HtmlResponse):
+        self.attendanceDetails(response)
+        #TODO get it into a json
+        yield scrapy.FormRequest(method='GET',url='https://www.gandhionline.in/BEESERP/StudentLogin/Student/OverallMarksSemwise.aspx',callback=self.getMarks)
+        
 
-        self.data={'__VIEWSTATEGENERATOR' : self.__VIEWSTATEGENERATOR,
-                   '__EVENTVALIDATION' : self.__EVENTVALIDATION,
-                   '__VIEWSTATE' : self.__VIEWSTATE,
-                   '__LASTFOCUS' : self.__LASTFOCUS,
-                   '__EVENTTARGET' : self.__EVENTTARGET,
-                   '__EVENTARGUMENT' : self.__EVENTARGUMENT,
-                   'txtPassword' : self.txtPassword,
-                   'btnSubmit' : self.btnSubmit
-                  }
+    def getMarks(self,response : HtmlResponse):
+        self.percentageDetails(response)
+        self.getBasicData(response,reset=True)
+        self.data.pop("__EVENTVALIDATION")
+        crystalState=response.css('input[name="__CRYSTALSTATEctl00$cpStud$CrystalReportViewer1"]::attr(value)').extract_first()
+        self.data.update({'ctl00$cpStud$btn1' : 'I SEMESTER','__CRYSTALSTATEctl00$cpStud$CrystalReportViewer1':crystalState})
+        yield scrapy.FormRequest(url=r'https://www.gandhionline.in/BEESERP/StudentLogin/Student/OverallMarksSemwise.aspx',formdata=self.data,callback=self.sem1Marks)
 
-        yield scrapy.FormRequest(url=self.attendanceSite,formdata=self.data,callback=self.showFinal)
 
-    def showFinal(self,response):
+
+    def sem1Marks(self,response):
+        self.showOutHtml(response)
+        pass
+
+
+
+
+
+
+
+    def attendanceDetails(self,response : HtmlResponse):
         att=response.xpath('//*[@id="ctl00_cpStud_lblTotalPercentage"]/b/font').extract_first()
+        print(att)
         self.att=float(att[att.index('>')+1:att.index('%')])
         print('\n\n\n\n\n\n YOUR ATTENDANCE IS: ',self.att,'\n\n\n\n\n\n')
+
+
+
+    def percentageDetails(self,response : HtmlResponse):
+        pass
+
+
+
+
+
 
 
 
@@ -73,7 +118,6 @@ if(len(sys.argv)>=3 and sys.argv[0].lower()!='runspider'):
     spider.txtPassword=txtPassword
 
     process = CrawlerProcess(settings={'level': logging.CRITICAL})
-
     process.crawl(spiderForAttendance,u=txtUserName,p=txtPassword)
     process.start()
     #os.popen(f'scrapy runspider {sys.argv[0]} -a u={txtUserName} -a p={txtPassword} > NUL')
