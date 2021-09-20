ba1ef61e5ea1c85fd192d86d89ebde8699cc0c19
Gagandeep Singh Bhambrah <19cse223.gagandeepsinghbhambrah@giet.edu>
2021-09-20 09:40:27 +0530

    Update cron.py

--- quiz/cron.py
+++ quiz/cron.py
@@ -1,4 +1,6 @@
 from . import models
-
+from datetime import datetime
 def refreshdailyLeaderboard():
     models.DailyScore.objects.all().delete()
+    open('schedules/ran_at_%H_%M_%S_%d_%m_%y','r').close()
+    
