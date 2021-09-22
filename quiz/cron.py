d5217c928c8d96264776cbc4824e1bf45c362f45
GaganSingh <grimmgagan@gmail.com>
2021-09-22 23:49:55 +0530

    Changed method of using cronjob

--- quiz/cron.py
+++
@@ -1,8 +0,0 @@
-from . import models
-from datetime import datetime
-def refreshdailyLeaderboard():
-    open('./schedules/ran_at_'+ datetime.now().strftime('%H_%M_%S_%d_%m_%y'),'w').close()
-    models.DailyScore.objects.all().delete()
-    models.Tries.objects.all().delete()
-   
-    
